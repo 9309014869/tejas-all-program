1c3a351533ca860c1301dc2a4b0f132aa48aa186
TEJAS GAJANAN KAWARKHE <112716745+9309014869@users.noreply.github.com>
2022-09-17 22:34:09 +0530

    Create local.java

---
+++ local.java
@@ -0,0 +1,14 @@
+
+ class Main{
+        void display(){
+            class Inner {
+                int y=98;
+                void innerdisplay(){
+                    System.out.println("jay gajanan");
+                    
+                } 
+            }
+            Inner sc=new Inner();
+            sc.innerdisplay();
+        }
+    }
