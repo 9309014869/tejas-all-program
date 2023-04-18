87c77cdd7d5f8f78ce3d2bb69bb86217593b7dfe
TEJAS GAJANAN KAWARKHE <112716745+9309014869@users.noreply.github.com>
2023-04-18 13:53:31 +0530

    Create cheackbox1.java

---
+++ cheackbox1.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import class C {
+    C(){
+    Fream f=new Fream("Checkbox Example");
+    CheckBox checkbox1=new Checkbox("c++");
+    chekbox1.setBounds(100,150,50,50);
+     CheckBox checkbox1=new Checkbox("java",true);
+     chekbox2.setBounds(100,150,50,50);
+     f.add(checkbox1);
+     f.add(checkbox);
+     f.Size(400,400);
+     f.setLayout(null);
+     f.setVisible(true);
+    }
+    public static void main(String args[]){
+        new C();
+    }
+}
